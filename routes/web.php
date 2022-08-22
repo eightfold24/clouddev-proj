cda96b32cfea51df4f3a4cc812a5effd03754726
Liam Riche <liamr@kainos.com>
2022-08-22 19:58:03 +0100

    add breeze to project

--- routes/web.php
+++ routes/web.php
@@ -13,48 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Models\Task;
-use Illuminate\Http\Request;
-
-/**
-    * Show Task Dashboard
-    */
 Route::get('/', function () {
-    error_log("INFO: get /");
-    return view('tasks', [
-        'tasks' => Task::orderBy('created_at', 'asc')->get()
-    ]);
+    return view('welcome');
 });
 
-/**
-    * Add New Task
-    */
-Route::post('/task', function (Request $request) {
-    error_log("INFO: post /task");
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-    if ($validator->fails()) {
-        error_log("ERROR: Add task failed.");
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-/**
-    * Delete Task
-    */
-Route::delete('/task/{id}', function ($id) {
-    error_log('INFO: delete /task/'.$id);
-    Task::findOrFail($id)->delete();
-
-    return redirect('/');
-});
+require __DIR__.'/auth.php';
